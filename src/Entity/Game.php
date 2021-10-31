9f83f1f21e434435cc826daa2b0cf098c0a8749f
Boubounech <bounechber@gmail.com>
2021-10-31 18:53:27 +0100

    Add Game Entity + create the database

---
+++ src/Entity/Game.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+//use App\Repository\GameRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ *
+ * @ApiResource(
+ *     collectionOperations={"get","post"},
+ *     itemOperations={"get", "put"}
+ * )
+ *
+ * @ORM\Entity(repositoryClass="App\Repository\GameRepository")
+ */
+
+class Game
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * The title of the game you're looking at
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $link;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $dlNumber;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $creator;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getDlNumber(): ?int
+    {
+        return $this->dlNumber;
+    }
+
+    public function setDlNumber(int $dlNumber): self
+    {
+        $this->dlNumber = $dlNumber;
+
+        return $this;
+    }
+
+    public function getCreator(): ?int
+    {
+        return $this->creator;
+    }
+
+    public function setCreator(int $creator): self
+    {
+        $this->creator = $creator;
+
+        return $this;
+    }
+}
